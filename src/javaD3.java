5cb5fea6bccec4b93e6efa65217c3e4522dddce3
Nishita <nishitaparija452@gmail.com>
2021-09-16 14:41:51 +0530

    Day 2 Added

---
+++ src/javaD3.java
@@ -0,0 +1,11 @@
+public class javaD3 { //code to demonstrate divisibility by 2
+    public static void main(String [] args)
+    {int num = 7;
+        if(num%2 == 0) {
+            System.out.println("Number Divisible by 2");
+        }
+        else {
+            System.out.println("Number is not divisible by 2");
+        }
+    }
+}
