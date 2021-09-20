74c0e6ed1516cde9ccba19e5b1689eef2973ab21
Nishita <nishitaparija452@gmail.com>
2021-09-20 19:52:03 +0530

    Day 7 Added

---
+++ src/javaD7.java
@@ -0,0 +1,10 @@
+public class javaD7 {
+    public static void main(String[] args){
+        int i = 1;
+        do {
+            System.out.println("&");
+            i++;
+        }
+        while (i < 6);
+    }
+}
